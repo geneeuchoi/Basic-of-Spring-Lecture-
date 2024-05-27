68167f9d7f71efa62c1f25877d8878f30ee6a37c
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-05-28 00:20:05 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -8,7 +8,11 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ComponentScan(
         //컴포넌트 스캔 대상에서 제외
+        basePackages = "hello.core.member", //찾는 시작 위치 지정. 멤버만 스캔 대상이 됨
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
+//지정안하면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
+//권장방법: 설정 정보 클래스의 위치를 프로젝트 최상단에 두고 패키지 위치를 지정x
 public class AutoAppConfig {
 }
